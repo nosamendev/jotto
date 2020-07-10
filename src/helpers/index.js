a42f238e506e39622eadbcd615330fc8324e0e63
nosamendev <nosamendev@gmail.com>
2020-07-10 21:29:49 +0300

    Setup Redux

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -7,6 +7,7 @@
 export function getLetterMatchCount(guessedWord, secretWord) {
   const secretLetterSet = new Set(secretWord.split(''));
   const guessedWordSet = new Set(guessedWord.split(''));
+
   return [...secretLetterSet].filter((letter) => guessedWordSet.has(letter))
     .length;
 }
---
+++ src/reducers/successReducer.js
@@ -0,0 +1,10 @@
+/**
+ * @function successReducer
+ * @param {array} state - Array of guessed words
+ * @param {obj} action - action to be reduced
+ * @returns {boolean} - new success state
+ */
+
+export default (state, action) => {
+  return null;
+};
