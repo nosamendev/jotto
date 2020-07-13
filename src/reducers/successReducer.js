f424a6e826a7424fe348f059161851879b250186
nosamendev <nosamendev@gmail.com>
2020-07-13 18:36:15 +0300

    Write code and tests for successReducer

--- src/reducers/successReducer.js
+++ src/reducers/successReducer.js
@@ -1,3 +1,4 @@
+import { actionTypes } from '../actions';
 /**
  * @function successReducer
  * @param {array} state - Array of guessed words
@@ -5,6 +6,11 @@
  * @returns {boolean} - new success state
  */
 
-export default (state, action) => {
-  return null;
+export default (state = false, action) => {
+  switch (action.type) {
+    case actionTypes.CORRECT_GUESS:
+      return true;
+    default:
+      return state;
+  }
 };
